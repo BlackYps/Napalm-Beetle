455da3db594ba2685886c4bac6c0038de9f956f2
BlackYps <jonas-fb@gmx.de>
2020-10-18 15:11:32 +0200

    Add initaial files

---
+++ mod_info.lua
@@ -0,0 +1,13 @@
+name = "Napalm beetle"
+uid = "eb1d1f6c-1136-11eb-adc1-0242ac120002"
+version = 1
+description = "Turns fire beetles into napalm bombs"
+author = "BlackYps"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+icon = ""
+before = {}
+after = {}
+conflicts = {}
